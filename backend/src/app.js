dd66ed5ecb2929e5057011426972cb67ab290e51
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-13 20:50:57 +0530

    implemented in app js

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const chatRouter = require('./routes/chat.routes.js');
+const errMiddleware = require('./middlewares/error.middleware.js');
+
+
 const app = express();
 
 
@@ -8,5 +11,8 @@ const app = express();
 app.use(express.json());
 app.use("/api/v1",chatRouter);
 
+
+
+app.use(errMiddleware);
 
 module.exports = app;
