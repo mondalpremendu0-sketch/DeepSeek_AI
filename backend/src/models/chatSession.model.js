cc3caec1ade4b89d2dd566964db245fd7c81c153
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-13 17:23:00 +0530

    setup user model

--- backend/src/models/chatSession.model.js
+++ backend/src/models/chatSession.model.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const ChatSessionSchema = new mongoose.Schema({
   userId: {
     type: mongoose.Schema.Types.ObjectId,
+    ref:"Users",
     required: true,
     index: true 
   },
---
+++ backend/src/models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const authSchema = mongoose.Schema(
+    {
+        firstname: {
+            type: String,
+            required: [true, "First name is required"]
+        },
+        lastname: {
+            type: String,
+            required: [true, "Last name is required"]
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, "Email is required"],
+            match: [
+                /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/,
+                "Email is invaild"
+            ]
+        },
+        // --- 2. Auth Method Tracking ---
+        authProvider: {
+            type: String,
+            enum: ["local", "google"],
+            required: true,
+            default: "local"
+        },
+        googleId: {
+            type: String,
+            unique: [true, "Google ID must be unique"],
+            sparse: true
+        },
+        profilePicture: {
+            type: String
+        },
+
+        password: {
+            type: String,
+            select: false,
+            minLength: [6, "Password atleast 6 charector"],
+            required: function () {
+                return this.authProvider === "local";
+            }
+        }
+    },
+    { timestamps: true }
+);
+
+const userModel = mongoose.model("Users", authSchema);
+
+module.exports = userModel;
