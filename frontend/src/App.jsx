e8947c905c134a5b12f2c68255c71b397c71c637
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-15 22:46:36 +0530

    connected to app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { RouterProvider } from "react-router/dom";
 import { router } from "./app.routes.jsx";
+import AuthProvider from "./features/auth/auth.context.jsx";
 import ChatProvider from "./features/chat/chat.context.jsx";
 
 function App() {
-    return(
-    <ChatProvider>
-        <RouterProvider router={router} />;
-    </ChatProvider>
-    )
+    return (
+        <AuthProvider>
+            <ChatProvider>
+                <RouterProvider router={router} />
+            </ChatProvider>
+        </AuthProvider>
+    );
 }
 
 export default App;
