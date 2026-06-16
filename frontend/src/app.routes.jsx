cffb022e4db55429b2958ee80112e59f200dc883
Premendu Mondal <mondalpremendu0@gmail.com>
2026-06-16 16:20:32 +0530

    implement register page

--- frontend/src/app.routes.jsx
+++ frontend/src/app.routes.jsx
@@ -1,11 +1,12 @@
 import { createBrowserRouter } from "react-router";
 import  Home from "./features/chat/pages/Home.jsx";
+import  Register from './features/auth/pages/Register.jsx'
 
 
 
 export const router = createBrowserRouter([
   {path:"/",element:<Home />},
-  {},
+  {path:"/register",element:<Register />},
   ])
   
   
